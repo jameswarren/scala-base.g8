ea79098fdce7f3d76ca13f0e413c0d8bea945f10
James Warren <james.warren@stanfordalumni.org>
2020-02-29 21:47:49 -0800

    update versions

--- src/main/g8/project/Plugins.scala
+++ src/main/g8/project/Plugins.scala
@@ -6,7 +6,7 @@ import org.scalastyle.sbt.ScalastylePlugin.autoImport.{scalastyle, scalastyleFai
 
 object Plugins {
   val ammoniteSettings = List(
-    libraryDependencies += "com.lihaoyi" % "ammonite" % "1.6.6" % "test" cross CrossVersion.full,
+    libraryDependencies += "com.lihaoyi" % "ammonite" % "2.0.4" % "test" cross CrossVersion.full,
     sourceGenerators in Test += Def.task {
       val file = (sourceManaged in Test).value / "amm.scala"
       IO.write(file, """object amm extends App { ammonite.Main().run() }""")
